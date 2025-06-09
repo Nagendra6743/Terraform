c7680ee4dff1ac2581dfbdb4f8cf210e2d034339
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-06-09 19:27:45 +0530

    Creating security-group

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "allow_everyone" {
   name        = "allow_all"
   description = "Allow TLS inbound traffic and all outbound traffic"
 
